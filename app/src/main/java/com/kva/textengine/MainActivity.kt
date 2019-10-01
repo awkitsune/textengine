948828f966b626e23df99fe9853d21faaebeab82
LowPolyFox <vladimir.kosickij@gmail.com>
2019-10-01 19:48:34 +0300

    Hello world commit
    it's... useless

---
+++ app/src/main/java/com/kva/textengine/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kva.textengine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
